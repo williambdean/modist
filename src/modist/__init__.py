7c94d308272f95e1d00a24a23df7cca6bd6808e9
Will Dean <wd60622@gmail.com>
2026-09-05 06:58:30 -0400

    bump version

--- src/modist/__init__.py
+++ src/modist/__init__.py
@@ -20,7 +20,7 @@ from .studentt import StudentT
 
 __all__ = ["Normal", "Beta", "Gamma", "StudentT", "DistMixin", "ui", "pymc"]
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 
 def __getattr__(name: str):
